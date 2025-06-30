f15acf4f5977d8cda9848f8862099c967134700f
ArcShahi <arcshahi54@gmail.com>
2025-06-30 14:23:14 +0530

    Solved 1823

---
+++ Solutions/1823.cpp
@@ -0,0 +1,14 @@
+// 1823. Find the Winner of the Circular Game
+
+#include <list>
+
+class Solution {
+public:
+     int findTheWinner(int n, int k) {
+          int player{ 0 };
+          for (int i{ 2 }; i <= n; ++i) {
+               player = (player + k) % i;
+          }
+          return player + 1;
+     }
+};
