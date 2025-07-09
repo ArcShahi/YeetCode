8fdc66e7db9ee68bf03a2b4b84bbdc6ac5ae82e3
ArcShahi <arcshahi54@gmail.com>
2025-07-09 09:10:59 +0530

    Solved 2810

---
+++ Solutions/2810.cpp
@@ -0,0 +1,22 @@
+// 2810. Faulty Keyboard
+
+#include <string>
+#include <algorithm>
+// TIME COMPLEXITY : O(n)
+// SPACE COMPLEXITY : O(n)
+
+class Solution {
+public:
+     std::string finalString(std::string s) {
+
+          std::string str{};
+          for (const auto c : s)
+          {
+               if (c == 'i')
+                    std::reverse(begin(str), end(str));
+               else
+                    str.push_back(c);
+          }
+          return str;
+     }
+};
