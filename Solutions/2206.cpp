6a9bbee6d3862ac66a6d70601305baff22173fe4
ArcShahi <arcshahi54@gmail.com>
2025-07-11 11:04:28 +0530

    Solved 2206

---
+++ Solutions/2206.cpp
@@ -0,0 +1,22 @@
+// 2206. Divide Array Into Equal Pairs
+
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+// TIME COMPLEXITY : O(N)
+// SPACE COMPPLEXITY : O(N)
+
+class Solution {
+public:
+     bool divideArray(std::vector<int>& nums) {
+
+          std::unordered_map<int, int> hashmap{};
+          for (const auto& i : nums)
+               ++hashmap[i];
+
+          return std::all_of(begin(hashmap), end(hashmap), [](const auto& e) {
+               return e.second % 2 == 0;
+               });
+     }
+};
