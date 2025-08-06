b09529c73c3f9fbdeac82d18d9c05f6a4a968be2
ArcShahi <arcshahi54@gmail.com>
2025-08-06 10:35:04 +0530

    Solved 1017

--- Solutions/1017.cpp
+++ Solutions/1017.cpp
@@ -3,4 +3,27 @@
 #include <string>
 #include <bitset>
 
+// TIME COMPLEXITY : O(logN)
+// SPACE COMPLEXITY : O(1)
 
+
+class Solution {
+public:
+     std::string baseNeg2(int n) {
+
+          std::string neg{};
+
+          while (n)
+          {
+               int rem{ n % -2 };
+               n /= -2;
+
+               if (rem < 0) {
+                    rem += 2;
+                    n += 1;
+               }
+               neg = char(rem + '0') + neg;
+          }
+          return neg;
+     }
+};
