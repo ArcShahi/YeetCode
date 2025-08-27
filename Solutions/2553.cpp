37489457265c2109e86ef666d8296f920a220724
ArcShahi <arcshahi54@gmail.com>
2025-08-27 18:52:44 +0530

    Solved 2553

--- Solutions/2553.cpp
+++ Solutions/2553.cpp
@@ -1,24 +1,24 @@
 // 2553. Separate the Digits in an Array
 
 #include <vector>
+#include <string>
+
+// TIME COMPLEXITY : O(N)
+// SPACE COMPELXITY : O(N)
 
 class Solution {
 public:
      std::vector<int> separateDigits(std::vector<int>& nums)
      {
-
           std::vector<int> splited{};
-          nums.reserve(nums.size() * 2); // assmuing every number is double digit
-
-
-          for (auto& i : nums)
+          splited.reserve(nums.size() * 2); // assmuing every number is double digit
+          for (auto& num : nums)
           {
-               int temp{ i };
-               
-
-
+               auto str{ std::to_string(num) };    
+               for (const auto c : str)
+                    splited.push_back(c - '0');         
           }
 
-
+          return splited;
      }
 };
