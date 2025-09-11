9c20a92a3acf2dab1e61388fa33fc950be721dfb
ArcShahi <arcshahi54@gmail.com>
2025-09-11 10:45:47 +0530

    Added Solutions

--- Solutions/2785.cpp
+++ Solutions/2785.cpp
@@ -3,6 +3,52 @@
 #include <string>
 #include <algorithm>
 #include <iterator>
+#include <array>
+
+// TIME COMPLEXITY : O(N)
+// SPACE COMPLEXITY : O(N)
+
+class Solution {
+public:
+     std::string sortVowels(std::string s) {
+          std::string vowels{};
+          vowels.reserve(s.length());
+
+          std::array<int, 52> freq{};
+
+          auto is_vowel = [](char x) { static const std::string v{ "aeiouAEIOU" };
+          return v.find(x) != std::string::npos; };
+
+
+          for (auto& c : s)
+          {
+               if (is_vowel(c)) {
+                    if (islower(c)) ++freq[c - 'a'];
+                    else ++freq[c - 'A' + 26];
+                    c = '#';
+               }
+          }
+
+          // uppercase vowels first
+          for (char c : std::string{ "AEIOU" }) {
+               vowels.append(freq[c - 'A' + 26], c);
+          }
+
+          // lowercase vowels
+          for (char c : std::string{ "aeiou" }) {
+               vowels.append(freq[c - 'a'], c);
+          }
+
+
+          auto it{ begin(vowels) };
+          std::ranges::for_each(s, [&](char& c)
+               {
+                  if (c == '#') c = *it++;
+               });
+        
+          return s;
+     }
+};
 
 
 // TIME COMPLEXITY : O(Nlogn)
@@ -11,13 +57,13 @@
 class Solution {
 public:
      std::string sortVowels(std::string s) {
-          std::string vowels{}; 
+          std::string vowels{};
           vowels.reserve(s.length());
 
           auto is_vowel{ [](char x) {static const std::string v{"aeiouAEIOU"};
                return v.find(x) != std::string::npos; } };
 
-          for (auto& c : s){
+          for (auto& c : s) {
                if (is_vowel(c)) {
                     vowels.push_back(c);
                     c = '#';
@@ -33,6 +79,8 @@ public:
           return s;
      }
 };
+
+
 
 
 
