dbe523e877848dfef2b20f7d0e58640cbb52ab62
ArcShahi <arcshahi54@gmail.com>
2025-09-13 12:41:01 +0530

    Solved Updated & Added new Solution

--- Solutions/105.cpp
+++ Solutions/105.cpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <unordered_map>
+#include <span>
 
  struct TreeNode {
      int val;
@@ -11,8 +12,11 @@
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
- // TIME COMPLEXITY :O(n)
- 
+
+ // ITERAIVE 
+ // TIME COMPLEXITY :O(N)
+ // SPACE COMPLEXITY : O(N)
+
  /*
    pop when going right  else insert as left child
  */
@@ -21,31 +25,65 @@ class Solution {
 public:
      TreeNode* buildTree(std::vector<int>& preorder, std::vector<int>& inorder) {
 
-          std::unordered_map<int, int> hashmap{};
-          for (int i{ 0 }; const auto & v : inorder)
-               hashmap[v] = i++;
+          std::unordered_map<int, int> pos{};
+          
+          for (int i{ 0 }; i < inorder.size(); ++i)
+               pos[inorder[i]] = i;
 
-          std::vector<TreeNode*> stack{};
+          std::vector<TreeNode*> stk{};
           auto root{ new TreeNode(preorder[0]) };
-          stack.push_back(root);
+          stk.push_back(root);
 
           for (int i{ 1 }; i < preorder.size(); ++i)
           {
                auto node{ new TreeNode(preorder[i]) };
                TreeNode* parent{ nullptr };
 
-               // pop the stack while new node should go right ( testing is right child )
-               while (!stack.empty() && hashmap[stack.back()->val]<hashmap[node->val])
+               // Find correct parent by popping nodes that should be ancestors
+               while (!stk.empty() && pos[stk.back()->val]< pos[node->val])
                {
-                    parent = stack.back();
-                    stack.pop_back();
+                    parent = stk.back();
+                    stk.pop_back();
                }
 
                if (parent) parent->right = node;
-               else stack.back()->left = node;
+               else stk.back()->left = node;
 
-               stack.push_back(node);
+               stk.push_back(node);
           }
           return root;
      }
+};
+
+// RECURSIVE
+// TIME COMPLEXITY : O(N)
+// SPACE COMPLEXITY: O(N)
+
+
+class Solution {
+public:
+     TreeNode* buildTree(std::vector<int>& preorder, std::vector<int>& inorder) {
+
+          for (int i{ 0 }; i < inorder.size(); ++i)
+               imap[inorder[i]] = i;
+
+          return make_tree(preorder, inorder);
+     }
+
+private:
+
+     std::unordered_map<int, int> imap{};
+
+     TreeNode* make_tree(std::span<int> pre, std::span<int> in)
+     {
+          if (pre.empty()) return nullptr;
+          auto root{ new TreeNode(pre[0]) };
+         // Convert from absolute position to relative position due to std::span
+          const int i{ imap[pre[0]] - imap[in[0]] }; // root index
+ 
+          root->left = make_tree(pre.subspan(1, i), in.first(i));
+          root->right = make_tree(pre.subspan(i + 1), in.subspan(i + 1));
+
+          return root;
+     }  
 };
