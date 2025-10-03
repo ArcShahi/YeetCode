a84efe5bcb34762f12f78ff69d6840df45945622
ArcShahi <arcshahi54@gmail.com>
2025-10-03 21:43:34 +0530

    Solved 2427

---
+++ Solutions/2427.cpp
@@ -0,0 +1,17 @@
+// 2427. Number of Common Factors
+
+#include <utility>
+
+// TIME COMPLEXITY : O(min(a,b))
+// SPACE COMPLEXITY : O(1)
+
+class Solution {
+public:
+     int commonFactors(int a, int b) {
+          int count{ 0 };
+          for (int i{ 1 }; i <=std::min(a,b); ++i)
+               if (a % i == 0 && b % i == 0) ++count;
+         
+          return count;
+     }
+};
