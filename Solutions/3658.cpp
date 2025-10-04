71856c067f142d811fbf6245e6e731a0b6fa77f1
ArcShahi <arcshahi54@gmail.com>
2025-10-04 17:33:06 +0530

    Updated Solution

--- Solutions/3658.cpp
+++ Solutions/3658.cpp
@@ -2,6 +2,21 @@
 
 #include <numeric>
 
+// TIME COMPLEXITY : O(1)
+// SPACE COMPLEXITY : O(1)
+
+class Solution {
+public:
+     int gcdOfOddEvenSums(int n) {
+
+          return n;
+
+          // Sum of even numbers : n(n+1)
+          // sum of odd number : n^2 
+          // Gretest number that can divide both : n^2+n and n^2 is n 
+     }
+};
+
 // TIME COMPLEXITY : O(Log(N^2));
 // SPACE COMPLEXITY: O(1)
 
