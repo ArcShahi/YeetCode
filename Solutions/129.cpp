d60de9bc1649aacb61db930a6b60486cb5cfe488
ArcShahi <arcshahi54@gmail.com>
2025-10-11 22:00:19 +0530

    Solved 129

---
+++ Solutions/129.cpp
@@ -0,0 +1,32 @@
+// 129. Sum Root to Leaf Numbers
+
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+ // TIME COMPLEXITY : O(N)
+ // SPACE COMPLEXITY : O(H)
+
+
+class Solution {
+public:
+     int sumNumbers(TreeNode* root) {
+          
+          return dfs(root, 0);
+     }
+private:
+     int dfs(TreeNode* root, int sum)
+     {
+          if (!root) return 0;
+          sum = (sum * 10) + root->val;
+
+          if (!root->left && !root->right) return sum;
+          return dfs(root->left, sum) + dfs(root->right, sum);
+     }
+};
