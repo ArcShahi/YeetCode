ec8dd8554e0ec88bcb9381350b1278bcb066c61b
ArcShahi <arcshahi54@gmail.com>
2025-10-14 21:12:45 +0530

    Solved 3099

--- Solutions/3099.cpp
+++ Solutions/3099.cpp
@@ -1,12 +1,12 @@
 // 3099. Harshad Number
 
+
 auto digit_sum(int x)
 {
      int sum{ 0 };
-     while (x){
+     for (; x; x /= 10)
           sum += x % 10;
-          x /= 10;
-     }
+
      return sum;
 }
 
